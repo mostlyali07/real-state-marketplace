3976de84eaf2715c4584650ebb02ffa286c6d035
Ali Rehan <104094853+mostlyali07@users.noreply.github.com>
2024-01-26 09:00:54 +0500

    update

--- client/src/components/OAuth.jsx
+++ client/src/components/OAuth.jsx
@@ -1,9 +1,22 @@
-import GoogleAuthProvider from '@firebase/auth'
+import { GoogleAuthProvider, getAuth, signInWithPopup } from '@firebase/auth'
+import { app } from '../firebase'
 
 export default function OAuth() {
     const handleGoogleClick = async () => {
         try {
             const provider = new GoogleAuthProvider()
+            const auth = getAuth(app)
+
+            const result = await signInWithPopup(auth, provider)
+
+            const res = await fetch('/api/auth/google', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ name: result.user.displayName, email: result.user.email, photo: result.user.photoURL })
+            })
+            const data = await res.json()
         } catch (error) {
             console.log("could not sign in with google", error);
         }
