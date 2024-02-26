9711f49d3b031cd6f14ca5fc30b297d885018d7c
Ali Rehan <104094853+mostlyali07@users.noreply.github.com>
2024-02-26 07:26:59 +0500

    complete image upload functionality

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -5,19 +5,18 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  // apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  // authDomain: "mern-estate-ab442.firebaseapp.com",
-  // projectId: "mern-estate-ab442",
-  // storageBucket: "mern-estate-ab442.appspot.com",
-  // messagingSenderId: "550168508311",
-  // appId: "1:550168508311:web:a3097766a1da1fced3b6d2",
-  apiKey: "AIzaSyAYuwCE0EN82ssHWdeT9FE098zZF-ubQPM",
-  authDomain: "real-estate-f87b2.firebaseapp.com",
-  projectId: "real-estate-f87b2",
-  storageBucket: "real-estate-f87b2.appspot.com",
-  messagingSenderId: "799257060398",
-  appId: "1:799257060398:web:8431778827fbe5c3b76bbf"
-
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: "mern-estate-ab442.firebaseapp.com",
+  projectId: "mern-estate-ab442",
+  storageBucket: "mern-estate-ab442.appspot.com",
+  messagingSenderId: "550168508311",
+  appId: "1:550168508311:web:a3097766a1da1fced3b6d2",
+  // apiKey: "AIzaSyAYuwCE0EN82ssHWdeT9FE098zZF-ubQPM",
+  // authDomain: "real-estate-f87b2.firebaseapp.com",
+  // projectId: "real-estate-f87b2",
+  // storageBucket: "real-estate-f87b2.appspot.com",
+  // messagingSenderId: "799257060398",
+  // appId: "1:799257060398:web:8431778827fbe5c3b76bbf"
 };
 
 // Initialize Firebase
